fe60a221ef376a7e978bdfadcf45ca9062ac136e
dsm23 <dsmurdoch357@gmail.com>
2017-05-10 09:47:57 +0100

    virtual machine test

---
+++ virtual machine/README.md
@@ -0,0 +1,5 @@
+# Test GitHub folder
+
+### by David Murdoch
+
+##### This folder does not have super cow powers
---
+++ virtual machine/test.java
@@ -0,0 +1,7 @@
+class test
+{
+	public staic void main(String[] args)
+	{
+		System.out.println("This is a GitHub test.");	
+	}
+}
